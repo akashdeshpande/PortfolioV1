726c6bb401306ff770db319fd74457265c6774f8
Akash <akashdeshpande118@gmail.com>
2021-04-15 19:35:15 +0530

    Added menu highlight on scroll section

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 ( () => {
-    //get hamburger and navLinks
+    // get hamburger and navLinks
     const ham = document.getElementById('hamburgerMenu');
     const nav = document.getElementById('navLinks');
 
@@ -7,4 +7,31 @@
     ham.addEventListener('click', () => {
         nav.classList.toggle('showNavLinks');
     })
+
+    
+    // for auto highlight menu
+    const sections = document.querySelectorAll('section');
+    const navLi = document.querySelectorAll('.navLink');
+
+    window.addEventListener('scroll', () => {
+        let currentSection = "";
+
+        // loop over for each section
+        sections.forEach ( section => {
+            const sectionTop = section.offsetTop;
+            const sectionHeight = section.clientHeight;
+
+            if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+                currentSection = section.getAttribute('id');
+            }
+        })
+
+        navLi.forEach (li => {
+            li.classList.remove('selectedNavLink');
+            if(li.classList.contains(currentSection)) {
+                li.classList.add('selectedNavLink');
+            }
+        });
+
+    });
 })();
